d8b7bbc492be322dc9c6c5dc9842fec5b6dadcc2
Thaanu Perera <thaanup@gmail.com>
2021-05-31 17:45:32 +0530

    Add cover section

--- src/components/home/cover-section.js
+++ src/components/home/cover-section.js
@@ -1,9 +1,27 @@
 import './cover-section.css';
 import coverImg from '../../assets/cover-image.png';
 
-function CoverSection(){
-    return(
-        <img class='cover-image' src={coverImg}></img>
+function CoverSection() {
+    return (
+        <div class='cover-section'>
+            <div class='cover-image'></div>
+            <div class='container'>
+                <div class='row cover-content'>
+                    <div class='col'>
+                        <div class='row'>
+                            <div class='col'>
+                                <div class='cover-text'>LOREM IPSUM<br />DOLOR SIT AMET</div>
+                            </div>
+                        </div>
+                        <div class='row'>
+                            <div class='col col-lg-2'>
+                                <button type="button" class="btn cover-button btn-outline-light">MORE</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
